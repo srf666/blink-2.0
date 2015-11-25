fa239b62d76a651405fe9a0447db47912f545e92
苏日甫 <1020343499@qq.com>
2015-11-25 20:28:27 +0800

    blink 2.0
    设置第十三引脚为输出端，因没有多余的发光二极管，第十三脚自带一个led灯，loop循环相当于一个死循环，每当运行完毕之后都会重新运行，让第十三个
    引脚输出高电平（灯亮）持续时间为1秒钟，然后让第十三引脚输出低电平（灯灭），持续时间为一秒。每次持续时间都会减少0.1秒，从led灯上看灯就会越
    闪越快。

---
+++ blink_2.0.ino
@@ -0,0 +1,48 @@
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);  
+  delay(1000);              
+  digitalWrite(13, LOW);    
+  delay(1000); 
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(900);
+  digitalWrite(13, HIGH);  
+  delay(1000);              
+  digitalWrite(13, LOW);    
+  delay(800); 
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(700);
+  digitalWrite(13, HIGH);  
+  delay(1000);              
+  digitalWrite(13, LOW);    
+  delay(600); 
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(400);
+  digitalWrite(13, HIGH);  
+  delay(1000);              
+  digitalWrite(13, LOW);    
+  delay(300); 
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(200);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(100);
+}
